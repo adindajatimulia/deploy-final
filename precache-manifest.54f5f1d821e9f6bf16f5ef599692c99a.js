0c8b162ed52a7c80b6550649cb01b238349f872d
adindajatimulia <adindajatii29@email.com>
2022-12-12 22:02:30 +0700

    first commit

---
+++ precache-manifest.54f5f1d821e9f6bf16f5ef599692c99a.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6a5bef3f42a51bb15c",
+    "url": "/css/app.e73b8e49.css"
+  },
+  {
+    "revision": "a75bd29884d14fb02cfb9e72b21154a5",
+    "url": "/img/background.jpg"
+  },
+  {
+    "revision": "7c0c874ae793ecc0a5e8a454610e0f59",
+    "url": "/img/cover.1.jpg"
+  },
+  {
+    "revision": "33a1eb25d7bbe30b4db4ecec480f098b",
+    "url": "/img/gelap.jpg"
+  },
+  {
+    "revision": "570ff5245a0e408866619278c17d4fd0",
+    "url": "/img/pages.zip"
+  },
+  {
+    "revision": "5399332a5658cf6e15f13b49d62727c3",
+    "url": "/img/pages/1.jpg"
+  },
+  {
+    "revision": "19ebf706bec8641924f9b04dd2dafbc6",
+    "url": "/img/pages/10.jpg"
+  },
+  {
+    "revision": "39e1591b22843817ae6f8bed50d3d0e2",
+    "url": "/img/pages/11.jpg"
+  },
+  {
+    "revision": "045659ff17f109ac9a554bb42fa112b1",
+    "url": "/img/pages/12.jpg"
+  },
+  {
+    "revision": "276e8471e4c5d9488be5b9c526845c69",
+    "url": "/img/pages/13.jpg"
+  },
+  {
+    "revision": "e2a6de242aaf709a156d17becbf13a96",
+    "url": "/img/pages/14.jpg"
+  },
+  {
+    "revision": "a417be5a5e0f69eda5cf4b86492f2b5e",
+    "url": "/img/pages/15.jpg"
+  },
+  {
+    "revision": "dcea3a3cd28217fe21132f68a5d99140",
+    "url": "/img/pages/16.jpg"
+  },
+  {
+    "revision": "07a3df1873d451b870767de3fb35dd88",
+    "url": "/img/pages/16_1.jpg"
+  },
+  {
+    "revision": "3335b7792f3a5d3e9ee15329109dea9f",
+    "url": "/img/pages/16_2.jpg"
+  },
+  {
+    "revision": "c412091b1ae38bfe1785687994dfc876",
+    "url": "/img/pages/17.jpg"
+  },
+  {
+    "revision": "2f79189fc5728088aac18b8ebaf41199",
+    "url": "/img/pages/18.jpg"
+  },
+  {
+    "revision": "3be4190a66ab14590a8ec0b50aecd90b",
+    "url": "/img/pages/19.jpg"
+  },
+  {
+    "revision": "29dcf62ff0432c9273a1b891054521d0",
+    "url": "/img/pages/2 (1).jpg"
+  },
+  {
+    "revision": "2bd3e87879eea6d131fe8f18d7424f4e",
+    "url": "/img/pages/2.jpg"
+  },
+  {
+    "revision": "957ad2db3f8d5b765591d97ee657d85c",
+    "url": "/img/pages/20.jpg"
+  },
+  {
+    "revision": "519d4c2f71a69168a38be963f22928ec",
+    "url": "/img/pages/21.jpg"
+  },
+  {
+    "revision": "61f0827be2ce49e660492d40c5666680",
+    "url": "/img/pages/22.jpg"
+  },
+  {
+    "revision": "9d4cdb42e67ffa41ad799969353cda7c",
+    "url": "/img/pages/23.jpg"
+  },
+  {
+    "revision": "1c5c450a7d8c9f09cd700aa8405773e6",
+    "url": "/img/pages/3.jpg"
+  },
+  {
+    "revision": "01e9156501fab3375b87463a22ab9658",
+    "url": "/img/pages/4.jpg"
+  },
+  {
+    "revision": "ba3338f95036eae61486857202a2f0d0",
+    "url": "/img/pages/5.jpg"
+  },
+  {
+    "revision": "69e61c8a05811411abf3706df2b2b013",
+    "url": "/img/pages/6.jpg"
+  },
+  {
+    "revision": "b3b5ef8dffbe3d94d88026ca15f7f9fc",
+    "url": "/img/pages/7.jpg"
+  },
+  {
+    "revision": "c46dee1d04317438cc5cd2da95ba816e",
+    "url": "/img/pages/8.jpg"
+  },
+  {
+    "revision": "ecd911bfc6a858c358eb788afc15800f",
+    "url": "/img/pages/9.jpg"
+  },
+  {
+    "revision": "7c0c874ae793ecc0a5e8a454610e0f59",
+    "url": "/img/pages/COVER.1.jpg"
+  },
+  {
+    "revision": "4eb73ffe076b703485cb15bf0708748d",
+    "url": "/img/terang.jpg"
+  },
+  {
+    "revision": "9626bd81b37d721c32e63ab254dd916d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e6a5bef3f42a51bb15c",
+    "url": "/js/app.24be5117.js"
+  },
+  {
+    "revision": "972ea3692c36f2265b4f",
+    "url": "/js/chunk-vendors.071ecea8.js"
+  },
+  {
+    "revision": "4e0ff27224983f096937849b6496e7db",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ead5c7cec5cc1b8b6120522928fddfc6",
+    "url": "/video/justrelax.mp3"
+  },
+  {
+    "revision": "538640017d6ec6bf1ef21f7b641b0c00",
+    "url": "/video/pleasecalmmymind.mp3"
+  },
+  {
+    "revision": "f766d6d9ffccae2c1796d1deca18f9a4",
+    "url": "/video/theweekend.mp3"
+  }
+]);
